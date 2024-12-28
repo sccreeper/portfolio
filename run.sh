b56c084a2abc7143816e6b464dc5b86f62e046f6
Oscar Peace <oscar@oscarcp.net>
2024-12-28 17:24:13 +0000

    fix: migration session cleanup

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "$DEBUG"
-
 if [ "$DEBUG" == "true" ]; then
+    echo "Running in debug mode"
     poetry run python3 -m src.app
 else
+    echo "Not running in debug mode"
     poetry run python3 -m gunicorn -b 0.0.0.0:8000 src.app:app
 fi
--- src/db/migration/versions/v0.py
+++ src/db/migration/versions/v0.py
@@ -82,4 +82,6 @@ def migrate():
 
         session.commit()
 
+    engine.dispose()
+
     
